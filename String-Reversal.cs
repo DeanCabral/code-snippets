367aacc3e110a3cee8bc35b0e9d12575aaa54cdf
Dean Cabral <u1503139@uel.ac.uk>
2017-09-10 18:44:21 +0100

    Create String-Reversal.cs

---
+++ String-Reversal.cs
@@ -0,0 +1,31 @@
+class StringReversal
+    {
+        static void Main(string[] args)
+        {
+            GetUserInput();
+        }
+
+        static void GetUserInput()
+        {
+            string input = "";
+
+            Console.Write("Enter a word: ");
+            input = Console.ReadLine();
+
+            Console.WriteLine("The reverse word of the string '{0}' is: {1}", input, ReverseString(input));
+
+            GetUserInput();
+        }
+
+        static string ReverseString(string input)
+        {
+            string reversed = "";
+
+            for (int i = input.Length - 1; i >= 0; i--)
+            {
+                reversed += input[i];
+            }
+
+            return reversed;
+        }
+    }
