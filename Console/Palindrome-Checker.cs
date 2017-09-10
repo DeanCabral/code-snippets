3d72c19dad1e2aa0703ccc09d5a344964cf4de8a
Dean Cabral <u1503139@uel.ac.uk>
2017-09-10 18:48:11 +0100

    Create Palindrome-Checker.cs

---
+++ Console/Palindrome-Checker.cs
@@ -0,0 +1,40 @@
+class Palindrome
+    {
+        static void Main(string[] args)
+        {
+            GetUserInput();
+        }
+
+        static void GetUserInput()
+        {
+            string input = "";
+            string output = "";
+
+            Console.Write("Enter a word: ");
+            input = Console.ReadLine();
+
+            output = Palindrome.IsPalindrome(input) ? "This word is a Palindrome. True." : "This word is not a Palindrome. False.";
+            Console.WriteLine(output);
+            Console.WriteLine();
+
+            GetUserInput();
+        }
+
+        static bool IsPalindrome(string word)
+        {
+            List<char> characters = new List<char>();
+            int count = 0;
+
+            foreach (char c in word)
+            {
+                characters.Add(c);
+            }
+
+            for (int i = 0; i < characters.Count; i++)
+            {
+                if (characters[i] == characters[(characters.Count - 1) - i]) count++;
+            }
+
+            return word.Length == count;
+        }
+    }
