e48cfa8f3c685e2b43f4f8737b0b26df5c6adc42
Dean Cabral <u1503139@uel.ac.uk>
2017-09-17 11:55:07 +0100

    Update Virtual-Assistant.cs

--- Console/Virtual-Assistant.cs
+++ Console/Virtual-Assistant.cs
@@ -32,7 +32,7 @@ class VirtualAssistant
 
             if (hour > 0 && hour < 12) return "Good Morning";
             else if (hour > 12 && hour < 18) return "Good Evening";
-            else if (hour > 18 && hour < 24) return "Good Night";
+            else if (hour > 18 && hour < 24) return "Greetings";
             else return "Hello";
         }
 
